4fa9a599778c477f3f33bb9369628994801b8065
larrycherry13 <laurent.chiricota@gmail.com>
2026-03-17 15:24:48 -0400

    Move Firebase config to environment variables for security

--- services/firebase.js
+++ services/firebase.js
@@ -4,12 +4,12 @@ import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRQ69j_25mPhr5e-LTSW4uNma6MwY1bbo",
-  authDomain: "glossia-325c1.firebaseapp.com",
-  projectId: "glossia-325c1",
-  storageBucket: "glossia-325c1.firebasestorage.app",
-  messagingSenderId: "765172556238",
-  appId: "1:765172556238:web:0452532025599e065bcb9f",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
